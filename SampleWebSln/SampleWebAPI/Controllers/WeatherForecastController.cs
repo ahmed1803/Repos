72a6db14c6f5d6884ac77194ce58d32dbb5da258
Ahmed <thaufeeqahmeds@hotmail.com>
2020-06-17 09:48:44 +0530

    update1

--- SampleWebSln/SampleWebAPI/Controllers/WeatherForecastController.cs
+++ SampleWebSln/SampleWebAPI/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace SampleWebAPI.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching","NA"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
